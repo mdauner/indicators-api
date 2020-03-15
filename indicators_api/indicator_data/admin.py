11bee02274e341db49dfb03ed8d8741891cb1d16
Michael Dauner <mdauner@gmail.com>
2020-03-15 18:41:12 +0000

    Add DataSet admin

--- indicators_api/indicator_data/admin.py
+++ indicators_api/indicator_data/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django import forms
+from django_admin_hstore_widget.forms import HStoreFormField
 
-# Register your models here.
+from .models import DataSet
+
+class DataSetAdminForm(forms.ModelForm):
+    data = HStoreFormField()
+    
+    class Meta:
+       model = DataSet
+       exclude = ()
+    
+
+@admin.register(DataSet)
+class DataSetAdmin(admin.ModelAdmin):
+    form = DataSetAdminForm
+    list_display = ('id', 'indicator', 'country', 'num_data',)
+    list_filter = ('indicator', 'country',)
+
+    def num_data(self, obj):
+        return len(list(filter(None, obj.data.values())))
+    num_data.short_description = 'Number of data points'
