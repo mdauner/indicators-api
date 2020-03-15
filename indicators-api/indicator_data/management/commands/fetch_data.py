611fdcffbd5bf49caaceab7d54235dc446119e2d
Michael Dauner <mdauner@gmail.com>
2020-03-15 17:41:12 +0000

    Save fetched data

--- indicators-api/indicator_data/management/commands/fetch_data.py
+++ indicators-api/indicator_data/management/commands/fetch_data.py
@@ -1,27 +1,37 @@
 from django.core.management.base import BaseCommand
 import requests
+import logging
 
+from ...models import DataSet
 
-COUNTRIES = ["usa", "mex", "ind", "chn", "nga"]
-INDICATORS = [
-    "SP.POP.TOTL",
-    "NY.GDP.MKTP.CD",
-    "EN.ATM.CO2E.PC",
-    "SP.DYN.LE00.IN",
-    "TX.VAL.TECH.MF.ZS",
-    "IP.PAT.NRES",
-    "IP.PAT.RESD",
-]
+logger = logging.getLogger("django")
+
 BASE_URL = "http://api.worldbank.org/v2/"
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        for indicator in INDICATORS:
-            for country in COUNTRIES:
-                url = f"{BASE_URL}country/{country}/indicator/{indicator}"
+        logger.info("Emptying table ...")
+        DataSet.objects.all().delete()
+
+        for indicator in DataSet.Indicator.choices:
+            for country in DataSet.Country.choices:
+                logger.info(
+                    f"Importing data for indicator '{indicator[1]}' ({country[1]}) ..."
+                )
+                url = f"{BASE_URL}country/{country[0]}/indicator/{indicator[0]}"
+
                 data = requests.get(
-                    url, params={"format": "json", "date": "1960:2018"},
+                    url,
+                    params={"format": "json", "date": "1960:2018", "per_page": "100"},
                 ).json()[1]
 
-                print(data)
+                DataSet.objects.create(
+                    country=country[0],
+                    indicator=indicator[0],
+                    data={point["date"]: point["value"] for point in data},
+                )
+
+        logger.info(
+            f"Fetched data for {len(DataSet.Indicator.choices)} indicators in {len(DataSet.Country.choices)} countries"
+        )
